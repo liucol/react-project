7de6b5695507ad384a878524eb3972a9b95b5760
杨柳 <1545766219@qq.com>
2017-04-20 12:27:41 +0800

    我的待办-第一阶段作品

--- src/App.js
+++ src/App.js
@@ -1,21 +1,79 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import 'normalize.css'
+import './reset.css'
+import './App.css'
+import TodoInput from './TodoInput'
+import TodoItem from './TodoItem'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            newTodo: '',
+            todoList: []
+        }
+    }
+
+    render() {
+        let todos = this.state.todoList.filter((item) => !item.deleted).map((item, index) => {
+            return ( // 为什么这里要加个括号？这是动手题3 🐸
+                <li key={index}>
+                    <TodoItem todo={item} onToggle={this.toggle.bind(this)} onDelete={this.delete.bind(this)}/>
+                </li>
+            )
+        })
+        return (
+            <div className="App">
+                <div className="head">
+                    <h1>My take Nodes</h1>
+                    <div className="inputWrapper">
+                        <TodoInput content={this.state.newTodo}
+                                   onChange={this.changeTitle.bind(this)}
+                                   onSubmit={this.addTodo.bind(this)}/>
+                    </div>
+                </div>
+                <ol className="todoList">
+                    {todos}
+                </ol>
+            </div>
+        )
+    }
+
+    toggle(e, todo) {
+        todo.status = todo.status === 'completed' ? '' : 'completed'
+        this.setState(this.state)
+    }
+
+    changeTitle(event) {
+        this.setState({
+            newTodo: event.target.value,
+            todoList: this.state.todoList
+        })
+    }
+
+    addTodo(event) {
+        this.state.todoList.push({
+            id: idMaker(),
+            title: event.target.value,
+            status: null,
+            deleted: false
+        })
+        this.setState({
+            newTodo: '',
+            todoList: this.state.todoList
+        })
+    }
+
+    delete(event, todo) {
+        todo.deleted = true
+        this.setState(this.state)
+    }
 }
 
 export default App;
+let id = 0
+
+function idMaker() {
+    id += 1
+    return id
+}
---
+++ src/TodoItem.js
@@ -0,0 +1,27 @@
+/**
+ * Created by yangliu on 2017/4/19.
+ */
+import React, {Component} from 'react';
+import './TodoItem.css'
+
+export default class TodoItem extends Component {
+    render() {
+        return (
+            <div className="TodoItem">
+                <input type="checkbox" checked={this.props.todo.status === 'completed'}
+                       onChange={this.toggle.bind(this)}/>
+                <span className="title">{this.props.todo.title}</span>
+                <div className="delet" onClick={this.delete.bind(this)}>删除</div>
+            </div>
+        )
+
+    }
+
+    toggle(e) {
+        this.props.onToggle(e, this.props.todo)
+    }
+
+    delete(e) {
+        this.props.onDelete(e, this.props.todo)
+    }
+}
